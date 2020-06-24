2a848812a5bfe741bba3f209fa112e4833d55610
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-24 03:19:40 +0000

    배열을 훼손하는 change  함수 만들기

--- main.c
+++ main.c
@@ -2,6 +2,43 @@
 
 #include <stdio.h>
 
+
+
+
+// 여기서 change 함수를 구현해주세요.
+void change(int* a) {
+
+
+  int size = sizeof(a) / sizeof(a[0]);
+
+  int b = *(a + 0);
+  int c = *(a + 1);
+  int sum = 0;
+  printf("%d\n", b);
+  printf("%d\n", c);
+
+
+  // for ( int i = 0; i < size; i++) {
+
+  //   *(a + i) = *(a + i) * 2;
+
+
+  // }
+
+
+  for ( int i = 0; i < size; i++) {
+
+    a[i] =  a[i] * 2;
+
+
+  }
+
+  
+  // 2가지 이상 방식으로
+}
+
+
+
 int main(void) {
   
   // int e = 50;
@@ -36,9 +73,9 @@ int main(void) {
   // 조건 : 변수를 2개만 사용해주세요.
   // 조건 : 선언을 제외한 배열 문법을 쓸 수 없습니다.
 
-  int arr[10];
-  int a = 0;
-  int size = sizeof(arr) / sizeof(arr[0]);
+  // int arr[10];
+  // int a = 0;
+  // int size = sizeof(arr) / sizeof(arr[0]);
   //v1 
   // 배열문법 사용해서 10개 받아 출력하기.
   // for(int i = 0; i < size; i++) {
@@ -58,18 +95,37 @@ int main(void) {
   //v2 
   // 배열문법 사용X 10개 받아 출력하기.
   // 배열변수의 의미에 대해 알아야 함.
-  for(int i = 0; i < size; i++) {
+  // for(int i = 0; i < size; i++) {
     
-    scanf("%d", &a);
+  //   scanf("%d", &a);
 
-    *(arr + i) = a;
+  //   *(arr + i) = a;
 
     
-  }
+  // }
 
-  for (int i = 0; i < 10; i++) {
-    printf("%d\n", *(arr + i));
-  }
+  // for (int i = 0; i < 10; i++) {
+  //   printf("%d\n", *(arr + i));
+  // }
+
+
+
+  // 문제 : 배열을 훼손하는 change 함수를 만들어주세요.
+
+  int x[2];
+  x[0] = 100;
+  x[1] = 200;
+  //int x[2] = {100, 200}; // 위 3 줄 코드의 같은 의미이다.
+  printf("%ld\n", (long)&x);
+  printf("%ld\n", (long)&x[1]);
+  printf("change 함수를 호출하기 전, x[0] : %d, x[1] : %d\n", x[0], x[1]);
+
+  // 여기서 change 함수를 실행해주세요.
+  change(x);
+
+  printf("change 함수를 호출하기 전, x[0] : %d, x[1] : %d\n", x[0], x[1]);
+  // 출력 => change 함수를 호출하기 전, x[0] : 200, x[1] : 400
+  
 
   return 0;
 }
